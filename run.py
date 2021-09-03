ffbc96a90b68a10eba4c6c05f4f985c12faae8eb
juliandavid-garcia <juliandavid.garcia@gmail.com>
2021-09-03 12:05:45 +0000

    Calculate surplus data

--- run.py
+++ run.py
@@ -64,10 +64,17 @@ def calculate_surplus_data(sales_row):
     - Positive surplus indicates waste
     - Negative surplus indicates extra made when stock was sold out.
     """
-print("Calculating surplus data...\n")
-stock = SHEET.worksheet("stock").get_all_values()
-stock_row = stock[-1]
-print(stock_row)
+    print("Calculating surplus data...\n")
+    stock = SHEET.worksheet("stock").get_all_values()
+    stock_row = stock[-1]
+
+    surplus_data = []
+    for stock,sales in zip(stock_row, sales_row):
+        surplus = int(stock) - sales
+        surplus_data.append(surplus)
+
+    return surplus_data
+
 def main():
     """
     Run all program functions
@@ -75,6 +82,8 @@ def main():
     data = get_sales_data()
     sales_data = [int(num) for num in data]
     update_sales_worksheet(sales_data)
-    calculate_surplus_data(sales_row)
+    new_surplus_data = calculate_surplus_data(sales_data)
+    print(new_surplus_data)
+
 print("Welcome To Lovesandwich Data Automation")
 main()
